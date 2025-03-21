86b9e72077add1cc22416897771482edeff0097e
Robert Freire <56230750+RobertFreire@users.noreply.github.com>
2025-03-21 15:22:38 -0300

    feat: Add Home Page

---
+++ src/pages/Logged/Home/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import AnimalCard from '../../../components/AnimalCard'
+import { AnimalsContainer } from './styles'
+
+const Home = () => {
+    return (
+        <AnimalsContainer>
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+        </AnimalsContainer>
+    )
+}
+
+export default Home
---
+++ src/pages/Logged/Home/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const AnimalsContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 4rem;
+    padding-top: 5rem;
+`;
