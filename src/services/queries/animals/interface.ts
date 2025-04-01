5f0bd0db32adf5b26c5d56b34b779f34210d4093
Robert Freire <56230750+RobertFreire@users.noreply.github.com>
2025-03-31 23:18:14 -0300

    feat: Add services animal

---
+++ src/services/queries/animals/interface.ts
@@ -0,0 +1,10 @@
+export interface Animal {
+    name: string
+    race: string
+    status: string
+    bornDate: string
+    observations: string
+    medicalNotes: string
+    sex: string
+    size: string
+}
