8aee8eda9e719253ea42584d8cb77fadd4d31a2d
Robert Freire <56230750+RobertFreire@users.noreply.github.com>
2025-04-08 15:17:57 -0300

    feat: Add Tab Items

---
+++ src/pages/Logged/Home/TabItems/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { ItemsContainer } from './styles'
+import AnimalCard from '../../../../components/AnimalCard'
+import { useQuery } from '@tanstack/react-query'
+import { GetDonateItems } from '../../../../services/queries/donateItems'
+import ItemCard from '../../../../components/ItemCard'
+
+const TabItems = () => {
+
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ['donated-items'],
+        queryFn: GetDonateItems,
+    })
+
+    if (isLoading) return <p>Carregando itens...</p>
+    if (isError || !data) return <p>Erro ao carregar os itens.</p>
+
+    return (
+        <ItemsContainer>
+            {data.map((donation) => (
+                <ItemCard item={donation.item} />
+            ))}
+        </ItemsContainer>
+    )
+}
+
+export default TabItems
---
+++ src/pages/Logged/Home/TabItems/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ItemsContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2rem;
+    padding-top: 5rem;
+`;
