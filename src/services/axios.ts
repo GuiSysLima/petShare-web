87e9b5ce6e1a4db4c51ec5b86f38554c28037703
Guilherme Rutemberg <95446385+GuiSysLima@users.noreply.github.com>
2025-05-08 16:21:34 -0300

    Update axios.ts vercel

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://petshare-api-production.up.railway.app",
+    baseURL: "https://pet-share-api.vercel.app",
 })
 
 export default api
